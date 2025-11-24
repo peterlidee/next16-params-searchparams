56a72dc0b65ca475f727dc97e327ebee9b2956ed
peterlidee <peter@lidee.be>
2025-11-24 11:40:48 +0100

    Updated tests for page.tsx

--- app/list/[listSlug]/__test__/page.test.tsx
+++ app/list/[listSlug]/__test__/page.test.tsx
@@ -23,4 +23,83 @@ describe('listSlug page component', () => {
     expect(ListControles).toHaveBeenCalledTimes(1);
     expect(screen.getByRole('list')).toBeInTheDocument();
   });
+
+  test('It displays "Invalid param" when the params in not in the data object', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'foobar' }),
+      searchParams: generateAsyncValue({}),
+    });
+    render(component);
+    expect(screen.getByText(/Invalid param./)).toBeInTheDocument();
+    // none of the other elements are present
+    expect(screen.queryByRole('heading', { level: 1 })).not.toBeInTheDocument();
+    expect(screen.queryByRole('link')).not.toBeInTheDocument();
+    expect(ListControles).toHaveBeenCalledTimes(0);
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
+  });
+
+  test('It renders the list asc when no searchParam sortOrder is present', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'fruit' }),
+      searchParams: generateAsyncValue({}),
+    });
+    render(component);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems[0]).toHaveTextContent(/apple/i);
+    expect(listItems[1]).toHaveTextContent(/banana/i);
+    expect(listItems[2]).toHaveTextContent(/cherry/i);
+  });
+
+  test('It renders the list asc when searchParam sortOrder is asc', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'fruit' }),
+      searchParams: generateAsyncValue({ sortOrder: 'asc' }),
+    });
+    render(component);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems[0]).toHaveTextContent(/apple/i);
+    expect(listItems[1]).toHaveTextContent(/banana/i);
+    expect(listItems[2]).toHaveTextContent(/cherry/i);
+  });
+
+  test('It renders the list asc when searchParam sortOrder is empty', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'fruit' }),
+      searchParams: generateAsyncValue({ sortOrder: '' }),
+    });
+    render(component);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems[0]).toHaveTextContent(/apple/i);
+    expect(listItems[1]).toHaveTextContent(/banana/i);
+    expect(listItems[2]).toHaveTextContent(/cherry/i);
+  });
+
+  test('It renders the list asc when searchParam sortOrder is multiple values', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'fruit' }),
+      searchParams: generateAsyncValue({ sortOrder: ['foo', 'bar'] }),
+    });
+    render(component);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems[0]).toHaveTextContent(/apple/i);
+    expect(listItems[1]).toHaveTextContent(/banana/i);
+    expect(listItems[2]).toHaveTextContent(/cherry/i);
+  });
+
+  test('It renders the list desc when searchParam sortOrder is desc', async () => {
+    const component = await ListPage({
+      params: generateAsyncValue({ listSlug: 'fruit' }),
+      searchParams: generateAsyncValue({ sortOrder: 'desc' }),
+    });
+    render(component);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(listItems[2]).toHaveTextContent(/apple/i);
+    expect(listItems[1]).toHaveTextContent(/banana/i);
+    expect(listItems[0]).toHaveTextContent(/cherry/i);
+  });
 });
