c45729b2ca1e676982f8e6d273b742ed7e5d7275
peterlidee <peter@lidee.be>
2025-11-26 15:37:11 +0100

    Reworked into basic example KISS

--- app/list2/[listSlug]/page.tsx
+++ app/list2/[listSlug]/page.tsx
@@ -1,51 +1,18 @@
 'use client';
 
-import ListControles from '@/components/ListControles';
-import { validateSortOrder } from '@/lib/validateSortOrder';
-import Link from 'next/link';
 import { use } from 'react';
 
-// PageProps (dunno)
-
-export type CustomPageProps = {
-  params: Promise<{ listSlug: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-};
-
-const data: Record<string, string[]> = {
-  fruit: ['apple', 'banana', 'cherry'],
-  names: ['Adam', 'Bob', 'Cole'],
-};
-
-const sortCallbacks = {
-  asc: (a: string, b: string) => (a > b ? 1 : -1),
-  desc: (a: string, b: string) => (a > b ? -1 : 1),
-};
-
-export default function List2Page({ params, searchParams }: CustomPageProps) {
-  const { listSlug } = use(params);
-  if (!(listSlug in data)) {
-    return <p>Invalid param.</p>;
-  }
-  const searchParamsResolved = use(searchParams);
-  const sortOrder = validateSortOrder(searchParamsResolved);
-
+export default function SyncUsePage({
+  params,
+  searchParams,
+}: PageProps<'/list/[listSlug]'>) {
+  const resolvedParams = use(params);
+  const resolvedSearchParams = use(searchParams);
   return (
     <div>
-      <Link href='/' className='inline-block underline text-blue-400 mb-4'>
-        home
-      </Link>
-      <h1 className='font-bold text-xl mb-2'>
-        List of {listSlug} (client component)
-      </h1>
-      <ListControles sortOrder={sortOrder} />
-      <ul>
-        {data[listSlug].sort(sortCallbacks[sortOrder]).map((item) => (
-          <li key={item} className='list-disc ml-3'>
-            {item}
-          </li>
-        ))}
-      </ul>
+      <h2>SyncUsePage</h2>
+      <div data-testid='params'>{resolvedParams.listSlug}</div>
+      <div data-testid='searchParams'>{resolvedSearchParams.sortOrder}</div>
     </div>
   );
 }
